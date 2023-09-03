f2056f2e50f449270ce47cde4adfcc67811e9d19
BuildTools <unconfigured@null.spigotmc.org>
2023-09-03 21:56:12 +0200

    improvement

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 import grafica.GestoreFinestra;
 
-public class MainGrafica {
+public class Main {
     public static void main(String[] args) {
         new GestoreFinestra();
     }
